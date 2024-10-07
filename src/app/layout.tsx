563e4fc55dae206f53893f6882528cafdab53664
Kashan Malik <sitesbykashan@gmail.com>
2024-10-07 21:07:01 +0500

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>AI $ ML BLOGS</title>
+        <title>AI & ML BLOGS</title>
         <link rel="shortcut icon" href="/favicon.jpg" type="image/x-icon" />
       </head>
       <body className='flex flex-col min-h-screen'>
